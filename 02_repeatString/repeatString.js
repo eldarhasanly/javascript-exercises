7021366f6a6a1bdcfd8dfec7143859bee8f443d3
eldarhasanly <151502641+eldarhasanly@users.noreply.github.com>
2025-01-20 17:10:11 +0400

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
+const repeatString = function (string, num) {
+    if (num < 0) return "ERROR";
+    let longString = "";
+    for (let i = 0; i < num; i++) {
+      longString += string;
+    }
+    return longString;
+  };
 
 // Do not edit below this line
 module.exports = repeatString;
