7d3d1686a147c5efaa75b7bd10aa78c9a5bca9d6
eldarhasanly <151502641+eldarhasanly@users.noreply.github.com>
2025-01-21 17:19:49 +0400

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,32 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let condition;
+
+    if (num1 < 0 || num2 < 0) return "ERROR";
+
+    if (num1 % 1 !== 0 || num2 % 1 !== 0) return "ERROR";
+
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+
+    if  (num1 > num2) {
+        condition = num1 - num2;
+
+        for (let i = 0; i <= condition; i++) {
+            sum += num2;
+            num2++;
+        }
+    
+        return sum;
+    } else {
+        condition = num2 - num1;
+
+        for (let i = 0; i <= condition; i++) {
+            sum += num1;
+            num1++;
+        }
+    
+        return sum;
+    }
 
 };
 
