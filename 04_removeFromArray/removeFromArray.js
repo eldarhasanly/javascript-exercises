53d03aef4ec3c7edc1f6ddacf51937de71a917f2
eldarhasanly <151502641+eldarhasanly@users.noreply.github.com>
2025-01-21 16:40:41 +0400

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...theArgs) {
+    const newArray = [];
+
+    array.forEach((item) => {
+        if (!theArgs.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
