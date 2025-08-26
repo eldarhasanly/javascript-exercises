803d3c4bd69841c711e26f268020d34b3d65ba43
eldarhasanly <151502641+eldarhasanly@users.noreply.github.com>
2025-08-26 19:15:50 +0400

    Finish the exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.trim();
 
+    const cleanTxt = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const reversedTxt = cleanTxt.split("").reverse().join("");
+
+    if (cleanTxt === reversedTxt) {
+      return true;
+    } else {
+      return false;
+    };
 };
 
 // Do not edit below this line
